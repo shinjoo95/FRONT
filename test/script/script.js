ac924fdf7e66eaef1529d196f16d965c7a35681d
shinjoo95 <shinjoo95@naver.com>
2022-07-19 14:40:14 +0900

    js

--- test/script/script.js
+++ test/script/script.js
@@ -1,14 +1,27 @@
-const spanEl = document.querySelector("main h2 span")
-const txtArr =['프론트엔드 개발자를 꿈꾸는 신주용입니다', '하루하루 발전하는 사람이 되겠습니다!', '잘 봐주세요😃']
-let index=0;
-let currentTxt = txtArr[index].split("");
-function writeTxt(){
-    spanEl.textContent += currentTxt.shift();
-    if(currentTxt.length !==0){
-        setTimeout(writeTxt, Math.floor(Math.random()%100));
-    }else{
+(function(){
+    const spanEl = document.querySelector("main h2 span");
+    const txtArr = ['프론트엔드 개발자를 꿈꾸는 신주용입니다', '어제보다 나은 개발자로 성장하겠습니다!', '잘 봐주세요😃'];
+    let index = 0;
+    let currentTxt = txtArr[index].split("");
+    function writeTxt(){
+      spanEl.textContent  += currentTxt.shift(); 
+      if(currentTxt.length !== 0){ 
+        setTimeout(writeTxt, Math.floor(Math.random() * 100));
+      }else{
         currentTxt = spanEl.textContent.split("");
         setTimeout(deleteTxt, 3000);
+      }
     }
-}
-writeTxt();
+    function deleteTxt(){
+      currentTxt.pop();
+      spanEl.textContent = currentTxt.join("");
+      if(currentTxt.length !== 0){
+        setTimeout(deleteTxt, Math.floor(Math.random() * 100))
+      }else{
+        index = (index + 1) % txtArr.length;
+        currentTxt = txtArr[index].split("");
+        writeTxt();
+      }
+    }
+    writeTxt();
+  })();
