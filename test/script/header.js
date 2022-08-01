67d07111332314dcae1a102048c3f11c2baa31c2
shinjoo95 <shinjoo95@naver.com>
2022-08-01 15:48:44 +0900

    js

--- test/script/header.js
+++ test/script/header.js
@@ -1,9 +1,14 @@
 const headerEl = document.querySelector("header");
 window.addEventListener('scroll', function(){
-  const browerScrollY = window.pageYOffset;
+this.requestAnimationFrame(scrollCheck);
+
+});
+function scrollCheck(){
+  let browerScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
   if(browerScrollY > 0){
     headerEl.classList.add("active");
   }else{
     headerEl.classList.remove("active");
-  }
-});
+}
+
+}
