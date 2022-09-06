9fd28bc4b379dbd88bc7c64f7c05ed286ede7ecc
shinjoo95 <shinjoo95@naver.com>
2022-09-06 17:43:20 +0900

    modal

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -11,7 +11,10 @@ function App() {
   //state 쓰는 이유 - 자주 변경될 것 같은 Html부분을 state로 만들어 놓기
   //일반 변수가 갑자기 변경이 되면 자동변경이 불가능함 
   //state는 갑자기 변경되면 자동으로 html에 재렌더링이 됨
- 
+  
+  let [modal, setModal] = useState(false);    // state 저장
+
+
   function 함수(){
     좋아요수(좋아요+1);
   }
@@ -53,11 +56,39 @@ function App() {
       <p>8월 5일 발행</p>
       </div>
       <div className="list">
-      <h4> {글제목[2]}</h4>
+      <h4 onClick={()=>{ setModal(!modal) }}> {글제목[2]}</h4>
       <p>8월 5일 발행</p>
       </div>
+
+      {/* 동적인 ui 만드는 step
+      1. html css로 미리 디자인 완성
+      2. ui 현재 상태를 state로 저장
+      3. state에 따라 ui가 어떻게 보일지 작성  */}
+      {
+        //삼항 연산자 조건식 ? 참일 때 실행할 코드 : 거짓일 때 실행할 코드 
+        modal == true ? <Modal/> : null
+      }
     </div>
   );
+}
+        /* 컴포넌트 만드는 법
+        function을 만들고 return()안에 html담기 
+        <함수명></함수명>쓰기 
+        컴포넌트 만들면 좋은 점
+        1. 반복적인 html 축약할 때
+        2. 큰 페이지에서 용이
+        3. 자주 변경되는 UI들 (성능향상) 
+        단점 : state 가져다쓸 때 문제생김, A함수에 있던 변수는 B함수에서 마음대로 가져다 쓸 수 없음 */
+
+  
+  function Modal(){
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
