4b7742810e85b78178c1c9557afd1f3abe8dcb5e
shinjoo95 <shinjoo95@naver.com>
2022-09-13 17:09:27 +0900

    router

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -7,43 +7,44 @@ import Col from 'react-bootstrap/Col';
 import { useState } from 'react';
 // import 작명 from './data.js';              //data.js 데이터를 가져옴
 import data from './data.js';
-import { Route, Routes, Link} from 'react-router-dom'
+import { Route, Routes, Link, useNavigate, Outlet} from 'react-router-dom'
+import Detail from './routes/Detail.js'
 function App() {
 
   let [shoes] = useState(data)
-
+  let navigate = useNavigate();
   return (
 
-    <div className="App">
-      {/* 페이지 나누는 법 */}
-      <Routes>
-        <Route path='/' element={<div>메인페이지임</div>}/>
-        <Route path='/detail' element={<div>상세페이지임</div>}/>
-      </Routes>
-      <Link to="/">메인페이지</Link>
-      <Link to="/detail">상세페이지</Link>
-
+    <div className="App">    
       {/* 대문자는 컴포넌트 */}
       <Navbar bg="dark" variant="dark"> 
         <Container>
           <Navbar.Brand href="#home">ShinShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      {/* 페이지 나누는 법 */}
+      {/* <Routes>
+        <Route path='/' element={<div>메인페이지임</div>}/>
+        <Route path='/detail' element={<div>상세페이지임</div>}/>
+      </Routes>
+      <Link to="/">메인페이지</Link>
+      <Link to="/detail">상세페이지</Link> */}
+
       {/* 이미지 넣는 법 */}
       {/* html에서 public 폴더 이미지 사용할 땐 그냥/이미지경로
       <img src='/logo192.png'/> 
       경로 지정 권장 방식 
       <img src={process.env.PUBLIC_URL + '/logo192.png'} /> */}
-      <div className='main-bg'></div>
+      {/* <div className='main-bg'></div> */}
       {/* html에서 src폴더의 이미지 넣을 경우 
         import도 추가해야 됨 */}
       {/* <div className='main-bg' style={{backgroundImage: 'url('+bg+')'}}></div> */}
-      <Container>
-      <Row>
+      {/* <Container>
+      <Row> */}
       {/* 컴포넌트 활용하기 */}
       {/* <Card shoes={shoes[0]} i={1}></Card>
       <Card shoes={shoes[1]} i={2}></Card>
@@ -59,16 +60,45 @@ function App() {
         <p>{shoes[2].price}</p>
         </Col> */}
         {/* 반복문 map 활용하기 */}
-        {
+        {/* {
           shoes.map((a, i)=>{
             return(
               <Card shoes={shoes[0]} i={i+1}></Card>
             )
           })
         }
-
+      </Row>
+      </Container> */}
+      <Routes>
+        <Route path='/' element={
+          <>
+          <div className='main-bg'></div>
+          <Container>
+          <Row>
+          {
+          shoes.map((a, i)=>{
+            return(
+              <Card shoes={shoes[0]} i={i+1}></Card>
+            )
+          })
+        }
       </Row>
       </Container>
+          </>
+        }/>
+        {/* /deatil/아무거나  :url 파라미터 */}
+        <Route path='/detail/:id' element={<Detail shoes={shoes}/>}/>
+
+        {/* Nested Routes 태그안의 태그 
+          outlet으로 위치 지정해줘야됨 */}
+        <Route path='/about' element={<About/>}>
+          <Route path='member' element={<div>멤버</div>}/>
+          <Route path='location' element={<div>위치</div>}/>
+        </Route>
+ 
+        {/* 404 page */}
+        <Route path='*' element={<div>없는 페이지 입니다.</div>}/>
+      </Routes>
     </div>
   );
 }
@@ -80,6 +110,15 @@ function Card(props){
   <h4>{props.shoes.title}</h4>
   <p>{props.shoes.price}</p>
   </Col>  
+  )
+}
+function About(){
+  return(
+    <div>
+      <h4>회사정보</h4>
+      {/* nested routes의 element 보여주는 곳은 <Outlet></Outlet> */}
+      <Outlet></Outlet>
+    </div>
   )
 }
 
